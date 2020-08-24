2bde5beefb854e78cc4a2841a4ea094689a44501
Kachanov-dev <vladimir@connectycube.com>
2020-08-24 19:22:15 +0300

    Fullscreen v1

--- videochat-conf/src/events-service.js
+++ videochat-conf/src/events-service.js
@@ -3,15 +3,15 @@ class uiUsersEvents {
   _renderSignalStat(userId) {
     if (window.ConnectyCubeActiveUserStatId !== userId) {
       if (window.ConnectyCubeActiveUserStatId !== null) {
-        this.toggleSelectedUserStatIcon(window.ConnectyCubeActiveUserStatId, false)
+        this.togglePopUp(window.ConnectyCubeActiveUserStatId, false)
       }
       window.ConnectyCubeActiveUserStatId = userId;
-      this.toggleSelectedUserStatIcon(window.ConnectyCubeActiveUserStatId, true)
+      this.togglePopUp(window.ConnectyCubeActiveUserStatId, true)
     }
   }
 
-  toggleSelectedUserStatIcon(userId, toShow) {
-    const $selectedUserStat = document.querySelector(`.display-signal-icon[data-id="${userId}"]`)
+  togglePopUp(userId, toShow) {
+    const $selectedUserStat = document.querySelector(`.display-popup[data-id="${userId}"]`)
     if (!$selectedUserStat) {
       return
     }
@@ -20,6 +20,23 @@ class uiUsersEvents {
     } else {
       $selectedUserStat.classList.remove('show')
     }
+  }
+
+  showFullscreen(userId) {
+    const $selectedFullScreen = document.getElementsByClassName(`stream-${userId}`)
+    const currentElem = $selectedFullScreen[0]
+
+    if (currentElem.requestFullscreen) {
+      currentElem.requestFullscreen();
+    } else if (currentElem.mozRequestFullScreen) { /* Firefox */
+      currentElem.mozRequestFullScreen();
+    } else if (currentElem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      currentElem.webkitRequestFullscreen();
+    } else if (currentElem.msRequestFullscreen) { /* IE/Edge */
+      currentElem.msRequestFullscreen();
+    }
+    const $selectedCloseFullScreen = document.getElementsByClassName(`display-full-screen-close`)
+    $selectedCloseFullScreen[0].classList.remove('display-full-screen-close-show')
   }
 }
 
