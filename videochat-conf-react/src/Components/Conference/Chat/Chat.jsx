328bb0380d0cfa663c39ff5db0967ad6a0f06ace
noblessem <ikondor@yahoo.com>
2022-03-22 13:27:36 +0100

    Resolvong conflicts

--- videochat-conf-react/src/Components/Conference/Chat/Chat.jsx
+++ videochat-conf-react/src/Components/Conference/Chat/Chat.jsx
@@ -37,7 +37,10 @@ const Chat = (props) => {
   const onSendMessage = (e) => {
     e.stopPropagation();
     if (messageRef.current.value.trim()) {
-      messageRef.current.value = messageRef.current.value.replace(" ", "");
+      messageRef.current.value = messageRef.current.value.replace(
+        /(\r\n|\n|\r)/gm,
+        ""
+      );
       chat.sendMessage(messageRef.current.value, props.dialog);
       messageRef.current.style.height = "45px";
 
@@ -89,7 +92,7 @@ const Chat = (props) => {
               ref={messageRef}
               onChange={messageArea}
               onKeyDown={onEnterPress}
-              onClick={(e) => {
+              onClic={(e) => {
                 e.stopPropagation();
               }}
               required={true}
