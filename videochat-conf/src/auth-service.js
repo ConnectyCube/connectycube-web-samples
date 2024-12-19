61d840d41270b9f8816c76334794855a407940db
Dave Lomber <admin@connectycube.com>
2024-12-19 13:13:23 +0200

    updated video conf sdample

--- videochat-conf/src/auth-service.js
+++ videochat-conf/src/auth-service.js
@@ -1,11 +1,8 @@
-import { credentials, appConfig } from './config';
+import { credentials, appConfig } from "./config";
+import RUG from "random-username-generator";
+import CryptoJS from "crypto-js";
 
 class AuthService {
-  $loginScreen = document.getElementById('login');
-  $callScreen = document.getElementById('call');
-  $loader = document.getElementById('login-loader');
-  $caption = document.getElementById('login-caption');
-
   init = (janusServerEndpoint = null) => {
     if (janusServerEndpoint) {
       appConfig.conference.server = janusServerEndpoint;
@@ -13,44 +10,32 @@ class AuthService {
     ConnectyCube.init(credentials, appConfig);
   };
 
-  createSession(user) {
-    return ConnectyCube.createSession(user);
+  async login(userName) {
+    const login = localStorage.getItem("userLogin") || RUG.generate();
+    const userProfile = {
+      login,
+      password: CryptoJS.MD5(login).toString(),
+      full_name: userName,
+    };
+
+    const session = await ConnectyCube.createSession(userProfile);
+    const user = session.user;
+
+    if (userName !== user.full_name) {
+      await ConnectyCube.users.update({ full_name: userName });
+    }
+
+    const chatCredentials = {
+      userId: user.id,
+      password: userProfile.password,
+    };
+    await ConnectyCube.chat.connect(chatCredentials);
+
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("userLogin", userProfile.login);
+
+    return user;
   }
-
-  login = (user) => {
-    return new Promise((resolve, reject) => {
-      this.$loader.classList.remove('hidden');
-      this.$caption.classList.add('hidden');
-
-      this.createSession(user)
-        .then(() =>
-          ConnectyCube.chat.connect({
-            userId: user.id,
-            password: user.password,
-          })
-        )
-        .then(() => {
-          this.hideLoginScreen();
-          this.$callScreen.classList.remove('hidden');
-          this.$loader.classList.add('hidden');
-          this.$caption.classList.remove('hidden');
-          resolve();
-        })
-        .catch(reject);
-    });
-  };
-
-  hideLoginScreen = () => {
-    this.$loginScreen.classList.add('hidden');
-  };
-
-  logout = () => {
-    ConnectyCube.chat.disconnect();
-    ConnectyCube.destroySession();
-
-    this.$callScreen.classList.add('hidden');
-    this.$loginScreen.classList.remove('hidden');
-  };
 }
 
 // create instance
