9e71a5f8b2e8ac03f42610c57710f80ec99afa3f
AntonSeleznev <anton@connectycube.com>
2024-12-26 14:08:08 +0200

    Refactor onboarding (#309)
    * refactor onboarding
    
    * update samples
    
    * update env
    
    * back
    
    * remove large file
    
    * remove endpoints
    
    * update
    
    * production: false

--- chat-react/src/config.js
+++ chat-react/src/config.js
@@ -1,15 +1,11 @@
 export const credentials = {
-  appId: process.env.REACT_APP_CONNECTYCUBE_APP_ID,
-  authKey: process.env.REACT_APP_CONNECTYCUBE_AUTH_KEY
+  appId: REPLACE_APP_ID,
+  authKey: 'REPLACE_APP_AUTH_KEY',
 };
 
 export const appConfig = {
   debug: {
     mode: 1,
-  },
-  endpoints: {
-    api: process.env.REACT_APP_CONNECTYCUBE_API_ENDPOINT,
-    chat: process.env.REACT_APP_CONNECTYCUBE_CHAT_ENDPOINT,
   },
   chat: {
     streamManagement: {
